3a12c1307dd0f2a469328abd907baeda8920de6d
MaysLastPlayYT <109207233+MaysLastPlayYT@users.noreply.github.com>
2022-12-25 17:06:22 +0200

    Add files via upload

--- assets/preload/data/genocide/script.lua
+++ assets/preload/data/genocide/script.lua
@@ -4,15 +4,7 @@ local continue = false;
 
 function opponentNoteHit()
     health = getProperty('health')
-    if getProperty('health') > 0.02 then
-        setProperty('health', health- 0.02);
+    if getProperty('health') > 0 then
+        setProperty('health', health- 0.007);
     end
-end
-function onCreate()
-
-	health = getProperty('health')
-
-	if getProperty('health') > 0.2 then
-	    setProperty('health', health+ 100)
-	end
 end
